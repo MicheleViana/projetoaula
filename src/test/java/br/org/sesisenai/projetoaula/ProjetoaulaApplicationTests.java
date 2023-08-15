45e675a7b2bccbd3b08fb00caed934b2371496f7
Michele <chelleviana23@gmail.com>
2023-08-15 13:21:46 -0300

    Adicionar coluna armazenar a data e incrementar juros

---
+++ src/test/java/br/org/sesisenai/projetoaula/ProjetoaulaApplicationTests.java
@@ -0,0 +1,15 @@
+package br.org.sesisenai.projetoaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoaulaApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoaulaApplicationTests .class, args);
+	}
+
+}
+
+
